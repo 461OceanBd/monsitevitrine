c0bb86e127a6551cb362b9863ed1ebf14ae5bf51
Romain Mercier <romain.mercier321@gmail.com>
2020-09-23 11:38:11 +0200

    Updates

---
+++ precache-manifest.194d3f0351cb688f79748e7e87e9ec86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62845b35595c154d49e5",
+    "url": "/monsitevitrine/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "62845b35595c154d49e5",
+    "url": "/monsitevitrine/static/js/main.f9e9ffc3.chunk.js"
+  },
+  {
+    "revision": "2142b1b738eafb42e36a",
+    "url": "/monsitevitrine/static/js/runtime~main.fb17622a.js"
+  },
+  {
+    "revision": "e771a1f8393f3b05bead",
+    "url": "/monsitevitrine/static/js/2.3d1987c1.chunk.js"
+  },
+  {
+    "revision": "c28aae54d5acdfa8b82276967550189c",
+    "url": "/monsitevitrine/index.html"
+  }
+];
